034a961200d2659eaa0024c627b938d7a7d44af5
Megan Sampson <msampson@nyc.yearup.org>
2022-11-17 11:03:24 -0500

    got radio buttons working

--- scripts/parks.js
+++ scripts/parks.js
@@ -5,116 +5,98 @@ window.onload = init;
 
 
 
-function init(){
+function init() {
     //FillDropDown
-fillDropdownForLocation()
-fillDropdownForParkType()
+    fillDropdownForLocation()
+    fillDropdownForParkType()
 
-selectedSeachType()
+    //Select Button
+    //    const selectParkTypebtn = document.getElementById("selectParkTypebtn");
+    //    selectParkTypebtn.onclick = selectParkTypebtnOnClick;
 
-   //Select Button
-//    const selectParkTypebtn = document.getElementById("selectParkTypebtn");
-//    selectParkTypebtn.onclick = selectParkTypebtnOnClick;
-
-   //Hide and Show Selected Search Options
+    //Hide and Show Selected Search Options
 }
 
 
 
 
-function fillDropdownForLocation(){
+function fillDropdownForLocation() {
 
-const searchLocationList = document.getElementById("searchLocationList");
+    const searchLocationList = document.getElementById("searchLocationList");
     let selectLocationOption = document.createElement("option");
     selectLocationOption.value = "";
     selectLocationOption.textContent = "Select State/Territory ..";
     searchLocationList.appendChild(selectLocationOption);
-    
-    
+
+
     let locationLength = locationsArray.length;
-    for(let i = 0 ; i < locationLength; i++){
+    for (let i = 0; i < locationLength; i++) {
         let locationNewOption = document.createElement("option");
         locationNewOption.value = locationsArray[i];
         locationNewOption.textContent = locationsArray[i];
- 
+
         searchLocationList.appendChild(locationNewOption);
     }
 }
 
-function fillDropdownForParkType(){
+function fillDropdownForParkType() {
     const searchParkTypeList = document.getElementById("searchParkTypeList");
     let selectParkTypeOption = document.createElement("option");
     selectParkTypeOption.value = "";
     selectParkTypeOption.textContent = "Select Park Type ..";
     searchParkTypeList.appendChild(selectParkTypeOption);
-    
-    
+
+
     let parkTypeLength = parkTypesArray.length;
-    for(let i = 0 ; i < parkTypeLength; i++){
+    for (let i = 0; i < parkTypeLength; i++) {
         let parkTypeNewOption = document.createElement("option");
         parkTypeNewOption.value = parkTypesArray[i];
         parkTypeNewOption.textContent = parkTypesArray[i];
- 
+
         searchParkTypeList.appendChild(parkTypeNewOption);
     }
 
 
 }
 
+
+let searchLocationRadio = document.getElementById("searchlocationradio");
+let searchParkTypeRadio = document.getElementById("searchparktyperadio");
+
+searchLocationRadio.onchange = selectedSearchType;
+searchParkTypeRadio.onchange = selectedSearchType;
+
 //Create a function to display list of options selected by the radio button 
-function selectedSeachType(){
-    let searchParkTypeList = document.getElementById("searchParkTypeList").style.display = "none";
-    let searchLocationList = document.getElementById("searchLocationList").style.display = "none";
-   
-    let searchLocationRadio = document.getElementById("searchlocationradio").checked;
-    let searchParkTypeRadio = document.getElementById("searchparktyperadio").checked;
+function selectedSearchType() {
+    let searchParkTypeList = document.getElementById("searchParkTypeList");
+    let searchLocationList = document.getElementById("searchLocationList");
 
-   if(searchLocationRadio == true){
-     searchLocationList.style.display = "block";
-     fillDropdownForParkType();
-  }
-    else if (searchParkTypeRadio == true) {
-     fillDropdownForLocation();
-     searchParkTypeList.style.display = "block";
+    if (searchLocationRadio.checked == true) {
+        searchLocationList.classList.replace("hide","show");
+        searchParkTypeList.classList.replace("show","hide");
+        fillDropdownForParkType();
     }
+    if (searchParkTypeRadio.checked == true) {
+        searchParkTypeList.classList.replace("hide","show")
+        searchLocationList.classList.replace("show","hide");
+        fillDropdownForLocation();
 
+    }
 }
 
 
 
 
 
-//Hide\Show Park Type List 
-function parkTypeShow(){
-  document.getElementById("searchParkTypeList").style.display = 'block';
-}
-
-function parkTypeHide(){
-    document.getElementById("searchParkTypeList").style.display = 'none';
-
-}
-
-//Hide\Show Location List 
-
-function locationShow(){
-  document.getElementById("searchLocationList").style.visibility = "visible";
-
-
-}
-
-function locationHide(){
-    document.getElementById("searchLocationList").style.visibility = "hidden";
-
-}
 
 
 // Output Park Information 
 
-function displayparksinResults(){
+function displayparksinResults() {
 
 }
 
-function displayparkinResults(){
+function displayparkinResults() {
 
 }
 
